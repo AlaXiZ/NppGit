a62ae302c15df1181e2f251ade7086b28c0953de
Schadin Alexey <schadin@gmail.com>
2016-02-06 10:51:09 +0300

    #17 модуль SQL IDE

---
+++ NppGitPlugin/Modules/SqlIde.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NppGit.Modules
+{
+    public class SqlIde : IModule
+    {
+        private IModuleManager _manager;
+
+        public bool IsNeedRun
+        {
+            get { return Settings.Modules.SQLIDE; }
+        }
+
+        public void Final()
+        {
+
+        }
+
+        public void Init(IModuleManager manager)
+        {
+            _manager = manager;
+        }
+    }
+}
