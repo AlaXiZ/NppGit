684073df469938bcadee5cd8d1ed6f5a7723dc2f
Schadin Alexey <schadin@gmail.com>
2016-11-15 12:43:09 +0300

    Начало доработки по интеграции с GitBash

---
+++ NppKate/Modules/GitFeatures/GitBash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NppKate.Common;
+
+namespace NppKate.Modules.GitFeatures
+{
+    public class GitBash : IModule
+    {
+        public bool IsNeedRun
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Final()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(IModuleManager manager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
